9c1659ebb0ede64d3f60359493d256484add8a41
tangym.edwin <tangym.edwin@gmail.com>
2017-02-05 22:27:35 -0500

    - Implemented mapping sql to web api - Implemented mapping python function to web api  Please enter the commit message for your changes. Lines starting

---
+++ api.py
@@ -0,0 +1,133 @@
+import json
+import re
+import inspect
+import importlib
+import sqlite3
+from flask import Flask, request, abort
+import yaml
+from attrdict import AttrDict
+
+with open('config.yml') as f:
+    config = AttrDict(yaml.load(f))
+
+DB = 'db.sqlite'
+app = Flask(__name__)
+init_sql = config.init.db
+init_module = config.init.module
+
+def execute(sql, params=[]):
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+    if type(sql) is str:
+        c.execute(sql, params)
+        if c.description:
+            names = [i[0] for i in c.description]
+            result = [dict(zip(names, record)) for record in c.fetchall()]
+        else:
+            result = c.fetchall()
+    else:
+        result = []
+        if not params:
+            params = [[] for s in sql]
+        for (s, param) in zip(sql, params):
+            c.execute(s, param)
+            if c.description:
+                names = [i[0] for i in c.description]
+                result += [[dict(zip(names, record)) for record in c.fetchall]]
+            else:
+                result += [c.fetchall()]
+    conn.commit()
+    conn.close()
+    return result
+
+# Detect whether `sql` can be fully parameterized by `param`
+def can_parameterize(sql, param):
+    try:
+        sql.format(**param)
+        return True
+    except KeyError:
+        return False
+
+# Detect whether `string` refers to a function in custom modules
+def is_python_function(string):
+    for module in init_module:
+        if re.match(r'^%s\.[a-zA-Z_][a-zA-Z0-9_]*$' % module, string.strip()):
+            return True
+    return False
+
+# Extract parameter values in requests
+def get_param_value(param_name):
+    result = {}
+    if type(param_name) is str:
+        param_name = [param_name]
+    for key in param_name:
+        value = request.args.get(key)
+        if request.json:
+            value = request.json.get(param_name) if not value else value
+        if value:
+            result[key] = value
+    return result
+
+
+# Handle requests according to methods defined in config.api[route]
+def handler(**kwargs):
+    route = request.url_rule.rule
+    method = request.method
+    for sql in config.api[route][method]:
+        if is_python_function(sql):
+            module = importlib.import_module(sql.split('.')[0])
+            try:
+                function = module.__dict__[sql.split('.')[1]]
+                params = inspect.getargspec(function)[0]
+                param_values = get_param_value(params)
+                return json.dumps(function(**param_values), indent=2)
+            except TypeError:
+                # Not enough parameters 
+                continue
+            except KeyError:
+                # Undefined function or possibly a sql 
+                pass
+        # print(route, method, sql)
+        params = re.findall(r'\{[a-zA-Z_][a-zA-Z0-9_]*\}', sql)
+        # remove `{` and `}`
+        params = [param[1:-1] for param in params]
+        param_values = get_param_value(params)
+        for key in param_values:
+            if key not in kwargs:
+                kwargs[key] = param_values[key]
+        if can_parameterize(sql, kwargs):
+            keys = [key[1:-1] for key in re.findall(r'\{[a-zA-Z_][a-zA-Z0-9_]*\}', sql)]
+            values = [kwargs[key] for key in keys]
+            sql = re.sub(r'\{[a-zA-Z_][a-zA-Z0-9_]*\}', '?', sql)
+            result = execute(sql, values)
+            return json.dumps(result, indent=2)
+    return json.dumps({'msg': 'Insufficient parameters.'}, indent=2)
+        
+# Parse multiple routes in one rule
+api = {}
+for url in config.api:
+    routes = re.split(r',\s+', url)
+    for route in routes:
+        route = route.strip()
+        if route not in api:
+            api[route] = {}
+        for method in config.api[url]:
+            if method not in api[route]:
+                api[route][method] = []
+            api[route][method] += config.api[url][method]
+config.update({'api': api})
+
+# Add url rule for each route
+for route in config.api:
+    # only choose one sql to execute which matches the parameter
+    handler.methods = [method for method in config.api[route]]
+    app.add_url_rule(route, '{}-{}'.format(route, handler), handler)
+
+
+if __name__ == "__main__":
+    execute(init_sql)
+    for module in init_module:
+        importlib.import_module(module)
+    app.run(host='0.0.0.0', debug=True)
+    pass
+
