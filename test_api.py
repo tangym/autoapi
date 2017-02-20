cfb4aac1fafb953d6ebbab2850d4e20b8c2a52ba
tangym <yut44@cse.psu.edu>
2017-02-20 06:34:43 +0000

    update test cases

--- test_api.py
+++ test_api.py
@@ -8,5 +8,9 @@ def test_root_404():
 def test_root():
     params = {'name': 'foo', 'age': 2}
     r = requests.get('http://localhost:5000', params=params)
+    assert r.json() is None
+
+def test_id():
+    r = requests.get('http://localhost:5000/1')
     ids = [item['id'] for item in r.json()].sort()
     assert ids == [1, 2, 3]
