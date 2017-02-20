c19aa9e642d396c503207abf363de1040aa8e04b
tangym <yut44@cse.psu.edu>
2017-02-20 06:31:13 +0000

    update test cases

--- test_api.py
+++ test_api.py
@@ -1,6 +1,12 @@
 import pytest
 import requests
 
-def test_url():
+def test_root_404():
     r = requests.get('http://localhost:5000')
-    assert r.status_code == 200 
+    assert r.status_code == 404
+
+def test_root():
+    params = {'name': 'foo', 'age': 2}
+    r = requests.get('http://localhost:5000', params=params)
+    ids = [item['id'] for item in r.json()].sort()
+    assert ids == [1, 2, 3]
